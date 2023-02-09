7cd0c61499630333756739645f95921f86405fe6
Bhavesh <bhavesh.atara@gmail.com>
2023-02-09 19:04:57 +0530

    v1

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build your project
+echo "Building project..."
+
+# Add other build commands here...
+
+# Output the build results
+echo "Build complete."
+
---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the tests
+echo "Running tests..."
+
+# Add your test commands here...
+
+# Output the test results
+echo "Tests complete."
+
